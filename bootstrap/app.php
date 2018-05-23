fee7c72a9c38e3a3feaf852a0ba30497d3003ecc
Tigran Vardanyan <tavardanyan@gmail.com>
2018-05-23 15:15:45 +0400

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,9 @@
 <?php
- header('Access-Control-Allow-Origin: *');
-header('Vary: Origin');
+    header('Access-Control-Allow-Origin: *');
+    header('Vary: Origin');
+    header('Access-Control-Allow-Methods : POST, GET, OPTIONS, PUT, DELETE, HEAD');
+    header('Allow: POST, GET, OPTIONS, PUT, DELETE, HEAD');
+    header('Access-Control-Allow-Headers : X-Requested-With, Content-Type, Authorization');
 
 if (PHP_SAPI != 'cli' && $_SERVER['REQUEST_METHOD']=='OPTIONS') {
     header('Access-Control-Allow-Methods : POST, GET, OPTIONS, PUT, DELETE, HEAD');
