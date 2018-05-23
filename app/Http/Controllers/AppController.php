f338a856f65975256df11aaf6bc7ffb7844828c7
Tigran Vardanyan <tavardanyan@gmail.com>
2018-05-23 17:08:17 +0400

    test 1

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AppController extends Controller
 {
     public function indexPage() {
-        return response()->json(['status' => 200, 'message' => 'succcesful', 'platform' => 'API']);
+        return response()->json(['status' => 200, 'message' => 'succcesful', 'platform' => 'my API']);
     }
 
     public function indexWebPage() {
